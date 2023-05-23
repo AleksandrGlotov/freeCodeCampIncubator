492b6f14229a706eaf56e05ff80e7db771cb9319
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-05-23 12:41:36 +0400

    fifth commit for sandbox 23.05

--- freeCodeCampFirstDay.js
+++ freeCodeCampFirstDay.js
@@ -185,3 +185,67 @@ function testElseIf(val) {
 }
 
 testElseIf(7);
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+  // Only change code above this line
+}
+
+testSize(7);
+
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  let result = strokes - par;
+
+  if (strokes == 1) {
+    return names[0];
+  } else if (result >= 3) {
+    return names[6];
+  } else if (result === 2) {
+    return names[5];
+  } else if (result === 1) {
+    return names[4];
+  } else if (result === 0) {
+    return names[3];
+  } else if (result === -1) {
+    return names[2];
+  } else {
+    return names[1];
+  }
+  // Only change code above this line
+}
+
+golfScore(5, 4);
