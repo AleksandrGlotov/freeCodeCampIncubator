9318654dacf637d5be0eafa34e93196d5f76efd9
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-05-24 12:44:52 +0400

    second commit for sandbox 24.05

--- freeCodeCampSecondDay.js
+++ freeCodeCampSecondDay.js
@@ -1,251 +1,97 @@
-function welcomeToBooleans() {
+function caseInSwitch(val) {
+  let answer = "";
   // Only change code below this line
-
-  return true; // Change this line
-
-  // Only change code above this line
-}
-
-function trueOrFalse(wasThatTrue) {
-  // Only change code below this line
-  if (wasThatTrue) {
-    return "Yes, that was true";
-  }
-  return "No, that was false";
-  // Only change code above this line
-}
-
-// Setup
-function testEqual(val) {
-  if (val == 12) {
-    // Change this line
-    return "Equal";
-  }
-  return "Not Equal";
-}
-
-testEqual(10);
-
-// Setup
-function testStrict(val) {
-  if (val === 7) {
-    // Change this line
-    return "Equal";
-  }
-  return "Not Equal";
-}
-
-testStrict(10);
-
-// Setup
-function compareEquality(a, b) {
-  if (a === b) {
-    // Change this line
-    return "Equal";
-  }
-  return "Not Equal";
-}
-
-compareEquality(10, "10");
-
-// Setup
-function testNotEqual(val) {
-  if (val != 99) {
-    // Change this line
-    return "Not Equal";
-  }
-  return "Equal";
-}
-
-testNotEqual(10);
-
-// Setup
-function testStrictNotEqual(val) {
-  if (val !== 17) {
-    // Change this line
-    return "Not Equal";
-  }
-  return "Equal";
-}
-
-testStrictNotEqual(10);
-
-function testGreaterThan(val) {
-  if (val > 100) {
-    // Change this line
-    return "Over 100";
-  }
-
-  if (val > 10) {
-    // Change this line
-    return "Over 10";
-  }
-
-  return "10 or Under";
-}
-
-testGreaterThan(10);
-
-function testGreaterOrEqual(val) {
-  if (val >= 20) {
-    // Change this line
-    return "20 or Over";
-  }
-
-  if (val >= 10) {
-    // Change this line
-    return "10 or Over";
-  }
-
-  return "Less than 10";
-}
-
-testGreaterOrEqual(10);
-
-function testLessThan(val) {
-  if (val < 25) {
-    // Change this line
-    return "Under 25";
-  }
-
-  if (val < 55) {
-    // Change this line
-    return "Under 55";
-  }
-
-  return "55 or Over";
-}
-
-testLessThan(10);
-
-function testLessOrEqual(val) {
-  if (val <= 12) {
-    // Change this line
-    return "Smaller Than or Equal to 12";
-  }
-
-  if (val <= 24) {
-    // Change this line
-    return "Smaller Than or Equal to 24";
-  }
-
-  return "More Than 24";
-}
-
-testLessOrEqual(10);
-
-function testLogicalAnd(val) {
-  // Only change code below this line
-
-  if (val <= 50 && val >= 25) {
-    return "Yes";
-  }
-
-  // Only change code above this line
-  return "No";
-}
-
-testLogicalAnd(10);
-
-function testLogicalOr(val) {
-  // Only change code below this line
-  if (val < 10 || val > 20) {
-    return "Outside";
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
   }
   // Only change code above this line
-  return "Inside";
+  return answer;
 }
 
-testLogicalOr(15);
+caseInSwitch(1);
 
-function testElse(val) {
-  let result = "";
+function switchOfStuff(val) {
+  let answer = "";
   // Only change code below this line
-
-  if (val > 5) {
-    result = "Bigger than 5";
-  } else {
-    result = "5 or Smaller";
-  }
-
-  // Only change code above this line
-  return result;
-}
-
-testElse(4);
-
-function testElseIf(val) {
-  if (val > 10) {
-    return "Greater than 10";
-  } else if (val < 5) {
-    return "Smaller than 5";
-  } else {
-    return "Between 5 and 10";
-  }
-}
-
-testElseIf(7);
-
-function orderMyLogic(val) {
-  if (val < 5) {
-    return "Less than 5";
-  } else if (val < 10) {
-    return "Less than 10";
-  } else {
-    return "Greater than or equal to 10";
-  }
-}
-
-orderMyLogic(7);
-
-function testSize(num) {
-  // Only change code below this line
-  if (num < 5) {
-    return "Tiny";
-  } else if (num < 10) {
-    return "Small";
-  } else if (num < 15) {
-    return "Medium";
-  } else if (num < 20) {
-    return "Large";
-  } else {
-    return "Huge";
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break;
   }
   // Only change code above this line
+  return answer;
 }
 
-testSize(7);
+switchOfStuff(1);
 
-const names = [
-  "Hole-in-one!",
-  "Eagle",
-  "Birdie",
-  "Par",
-  "Bogey",
-  "Double Bogey",
-  "Go Home!",
-];
-
-function golfScore(par, strokes) {
+function sequentialSizes(val) {
+  let answer = "";
   // Only change code below this line
-  let result = strokes - par;
-
-  if (strokes == 1) {
-    return names[0];
-  } else if (result >= 3) {
-    return names[6];
-  } else if (result === 2) {
-    return names[5];
-  } else if (result === 1) {
-    return names[4];
-  } else if (result === 0) {
-    return names[3];
-  } else if (result === -1) {
-    return names[2];
-  } else {
-    return names[1];
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
   }
   // Only change code above this line
+  return answer;
 }
 
-golfScore(5, 4);
+sequentialSizes(1);
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
