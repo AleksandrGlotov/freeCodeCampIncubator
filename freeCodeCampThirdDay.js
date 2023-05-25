ae0601611f540253ec71cef69eb6975ee26f2d72
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-05-25 13:18:22 +0400

    fourth commit for sandbox 25.05

--- freeCodeCampThirdDay.js
+++ freeCodeCampThirdDay.js
@@ -150,3 +150,60 @@ function rangeOfNumbers(startNum, endNum) {
     return numbers;
   }
 }
+
+function checkScope() {
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  // Using s = [2, 5, 7] would be invalid
+  return s;
+  // Only change code above this line
+}
+editInPlace();
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+const magic = () => new Date();
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+const sum = (...args) => {
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+};
