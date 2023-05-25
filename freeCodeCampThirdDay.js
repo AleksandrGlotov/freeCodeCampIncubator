45838fffa96c1c1c4d7218ab6826531d43b7cded
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-05-25 12:54:37 +0400

    third commit for sandbox 25.05

--- freeCodeCampThirdDay.js
+++ freeCodeCampThirdDay.js
@@ -111,9 +111,42 @@ function convertToInteger(str) {
 
 convertToInteger("56");
 
-
 function convertToInteger(str) {
-  return parseInt(str, 2)
+  return parseInt(str, 2);
 }
 
 convertToInteger("10011");
+
+function checkEqual(a, b) {
+  return a == b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
+
+function checkSign(num) {
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+
+checkSign(10);
+
+// Only change code below this line
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
+// Only change code above this line
+
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
