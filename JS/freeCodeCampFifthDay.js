7047eb333ed53f81b64cfb414e570131b8e984af
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-05-27 11:03:55 +0400

    second commit for sandbox 27.05

--- JS/freeCodeCampFifthDay.js
+++ JS/freeCodeCampFifthDay.js
@@ -25,3 +25,27 @@ let result4 = calRegex.test(rickyAndCal);
 let caboose = "The last car on a train is the caboose";
 let lastRegex = /caboose$/; // Change this line
 let result5 = lastRegex.test(caboose);
+
+let quoteSample1 = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // Change this line
+let result6 = quoteSample1.match(alphabetRegexV2).length;
+
+let quoteSample2 = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // Change this line
+let result7 = quoteSample2.match(nonAlphabetRegex).length;
+
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result8 = movieName.match(numRegex).length;
+
+let movieName1 = "2001: A Space Odyssey";
+let noNumRegex = /\D/g; // Change this line
+let result9 = movieName1.match(noNumRegex).length;
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
+let result10 = userCheck.test(username);
+
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result11 = sample.match(countWhiteSpace);
