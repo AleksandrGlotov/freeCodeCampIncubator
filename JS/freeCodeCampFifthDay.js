7584077239bacf02fcab1b59ece2f2a9db18fa15
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-05-27 11:38:14 +0400

    third commit for sandbox 27.05

--- JS/freeCodeCampFifthDay.js
+++ JS/freeCodeCampFifthDay.js
@@ -49,3 +49,32 @@ let result10 = userCheck.test(username);
 let sample = "Whitespace is important in separating words";
 let countWhiteSpace = /\s/g; // Change this line
 let result11 = sample.match(countWhiteSpace);
+
+let sample1 = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // Change this line
+let result12 = sample1.match(countNonWhiteSpace);
+
+let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6} no/; // Change this line
+let result13 = ohRegex.test(ohStr);
+
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/; // Change this line
+let result14 = haRegex.test(haStr);
+
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/; // Change this line
+let result15 = timRegex.test(timStr);
+
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result16 = favRegex.test(favWord);
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6})(?=\w*\d{2})/;
+let result17 = pwRegex.test(sampleWord);
+
+let myString = "Eleanor Roosevelt";
+let myRegex3 = /(Franklin|Eleanor) (([A-Z]\.?|[A-Z][a-z]+) )?Roosevelt/;
+let result18 = myRegex3.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
