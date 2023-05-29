5665ea91f74d85fc5e855e72e94cd9a432d88712
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-05-29 12:27:54 +0400

    third commit for sandbox 29.05

--- JS/freeCodeCampSixthDay.js
+++ JS/freeCodeCampSixthDay.js
@@ -72,3 +72,118 @@ function countToFive() {
 }
 
 countToFive();
+
+function zeroArray(m, n) {
+  // Creates a 2-D array with m rows and n columns of zeroes
+  let newArray = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+    let row = [];
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row.push(0);
+    }
+    // Pushes the current row, which now has n zeroes in it, to the array
+    newArray.push(row);
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+function myFunc() {
+  for (let i = 1; i <= 4; i += 2) {
+    console.log("Still going!");
+  }
+}
+
+let yourArray = [1, "kal", true, false, 2]; // Change this line
+
+let myArray1 = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray1[1] = 2;
+// Only change code above this line
+console.log(myArray1);
+
+function mixedNumbers(arr) {
+  // Only change code below this line
+  arr.push(7, "VIII", 9);
+  arr.unshift("I", 2, "three");
+  // Only change code above this line
+  return arr;
+}
+
+console.log(mixedNumbers(["IV", 5, "six"]));
+
+function popShift(arr) {
+  let popped = arr.pop(); // Change this line
+  let shifted = arr.shift(); // Change this line
+  return [shifted, popped];
+}
+
+console.log(popShift(["challenge", "is", "not", "complete"]));
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(0, 1);
+arr.splice(3, 8);
+// Only change code above this line
+console.log(arr);
+
+function htmlColorNames(arr) {
+  // Only change code below this line
+  arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+  // Only change code above this line
+  return arr;
+}
+
+console.log(
+  htmlColorNames([
+    "DarkGoldenRod",
+    "WhiteSmoke",
+    "LavenderBlush",
+    "PaleTurquoise",
+    "FireBrick",
+  ])
+);
+
+function forecast(arr) {
+  // Only change code below this line
+  arr = arr.slice(2, 4);
+  return arr;
+}
+
+// Only change code above this line
+console.log(
+  forecast(["cold", "rainy", "warm", "sunny", "cool", "thunderstorms"])
+);
+
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // Only change code below this line
+    newArr.push(...arr);
+    // Only change code above this line
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
+
+function spreadOut() {
+  let fragment = ["to", "code"];
+  let sentence = ["learning", ...fragment, "is", "fun"]; // Change this line
+  return sentence;
+}
+
+console.log(spreadOut());
+
+function quickCheck(arr, elem) {
+  // Only change code below this line
+  return arr.indexOf(elem) != -1 ? true : false;
+  // Only change code above this line
+}
+
+console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
