d512d5cce4b3257ea627492d952b1308b6a4ea63
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-06-02 13:14:16 +0400

    second commit for sandbox 2.06

--- JS/freeCodeCampTenthDay.js
+++ JS/freeCodeCampTenthDay.js
@@ -49,3 +49,61 @@ whatIsInAName(
   { last: "Capulet" }
 );
 whatIsInAName([{ apple: 1 }, { apple: 1 }, { apple: 1, bat: 2 }], { apple: 1 });
+
+function spinalCase(str) {
+  return str
+    .replace(/[^A-Za-z]+/g, " ")
+    .split(/\s|(?=[A-Z])/)
+    .map((i) => i[0].toLowerCase() + i.slice(1))
+    .join("-");
+}
+spinalCase("This Is Spinal Tap");
+spinalCase("The_Andy_Griffith_Show");
+spinalCase("AllThe-small Things");
+spinalCase("thisIsSpinalTap");
+
+function translatePigLatin(str) {
+  if (/[aeiou]/.test(str[0])) {
+    return str + "way";
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      if (/[aeiou]/.test(str[i])) {
+        return str.slice(i) + str.slice(0, i) + "ay";
+      }
+    }
+  }
+  if (!/[aeiou]/.test(str)) {
+    return str + "ay";
+  }
+}
+
+translatePigLatin("consonant");
+
+function myReplace(str, before, after) {
+  if (before[0] == before[0].toUpperCase()) {
+    after = after[0].toUpperCase() + after.slice(1)
+  } else {
+    after = after[0].toLowerCase() + after.slice(1)
+  }
+  return str.replace(before, after)
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+function pairElement(str) {
+  let result = []
+  str.split("").map(i => {
+    if (i == "A") {
+      result.push(["A", "T"])
+    } else if (i == "T") {
+      result.push(["T", "A"])
+    } else if (i == "C") {
+      result.push(["C", "G"])
+    } else {
+      result.push(["G", "C"])
+    }
+  })
+  return result;
+}
+
+pairElement("GCG");
