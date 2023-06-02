63718ea931987fb59d724d6aa1f53908a74f5af2
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-06-02 15:43:08 +0400

    third commit for sandbox 2.06

--- JS/freeCodeCampTenthDay.js
+++ JS/freeCodeCampTenthDay.js
@@ -81,29 +81,80 @@ translatePigLatin("consonant");
 
 function myReplace(str, before, after) {
   if (before[0] == before[0].toUpperCase()) {
-    after = after[0].toUpperCase() + after.slice(1)
+    after = after[0].toUpperCase() + after.slice(1);
   } else {
-    after = after[0].toLowerCase() + after.slice(1)
+    after = after[0].toLowerCase() + after.slice(1);
   }
-  return str.replace(before, after)
+  return str.replace(before, after);
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
 
 function pairElement(str) {
-  let result = []
-  str.split("").map(i => {
+  let result = [];
+  str.split("").map((i) => {
     if (i == "A") {
-      result.push(["A", "T"])
+      result.push(["A", "T"]);
     } else if (i == "T") {
-      result.push(["T", "A"])
+      result.push(["T", "A"]);
     } else if (i == "C") {
-      result.push(["C", "G"])
+      result.push(["C", "G"]);
     } else {
-      result.push(["G", "C"])
+      result.push(["G", "C"]);
     }
-  })
+  });
   return result;
 }
 
 pairElement("GCG");
+
+function fearNotLetter(str) {
+  for (let i = 1; i < str.length; i++) {
+    if (str[i].charCodeAt() - str[i - 1].charCodeAt() != 1) {
+      return String.fromCharCode(str[i].charCodeAt() - 1);
+    }
+  }
+}
+
+fearNotLetter("abce");
+
+function uniteUnique(arr) {
+  let result = [...arr];
+  for (let i = 1; i < arguments.length; i++) {
+    result.push(...arguments[i]);
+  }
+  return [...new Set(result)];
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+function convertHTML(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/>/g, "&gt;")
+    .replace(/</g, "&lt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+convertHTML("Dolce & Gabbana");
+convertHTML("Hamburgers < Pizza < Tacos");
+convertHTML('Stuff in "quotation marks"');
+
+function sumFibs(num) {
+  let arr = [0, 1, 1];
+  if (num == 1) {
+    return num;
+  } else {
+    for (let i = 2; i <= num; i++) {
+      if (arr[arr.length - 1] + arr[arr.length - 2] == i) {
+        arr.push(i);
+      }
+    }
+    return arr.filter((i) => i % 2 == 1).reduce((sum, acc) => sum + acc, 0);
+  }
+}
+
+sumFibs(10);
+sumFibs(13);
+sumFibs(1000);
