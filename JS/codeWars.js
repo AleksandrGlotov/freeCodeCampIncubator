54606bb41323affcec9f462d2589306be0f2d9ba
AleksandrGlotov <107253003+AleksandrGlotov@users.noreply.github.com>
2023-06-07 13:11:08 +0400

    third commit for sandbox 7.06

--- JS/codeWars.js
+++ JS/codeWars.js
@@ -8,3 +8,11 @@ function sortArray(array) {
   const odds = array.filter((x) => x % 2).sort((a, b) => a - b);
   return array.map((x) => (x % 2 ? odds.shift() : x));
 }
+
+function findMissingLetter(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i + 1].charCodeAt() - array[i].charCodeAt() > 1) {
+      return String.fromCharCode(array[i].charCodeAt() + 1);
+    }
+  }
+}
